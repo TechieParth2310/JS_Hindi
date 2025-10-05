e236394896834daa2dca7e190073b553c4324238
TechieParth2310 <parthkothawade2310@gmail.com>
2025-10-05 20:05:41 +0000

    02_datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 92763
+let accountEmail = "xyz@gmail.com"
+var accountPassword ="1234"
+accountCity = "Jaipur"
+let accountState;
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as a newer version
+
+// alert(3+3) // we are using nodejs and not the browser so it won't run
+
+let name ="Parth" //string
+let age = 22. // number
+let isLoggedIn = false // boolean
+
+// null => standalone value;
+// undefined
+//symbol => unique
+
+// These above all are the primitive datatypes.
+
+// object
+
+console.log(typeof undefined) // => returns undefined only as a datatype 
+console.log(typeof null) // => returns object as a datatype // imp Interview Q
